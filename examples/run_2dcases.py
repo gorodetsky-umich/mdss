922316aba38fdb303da323588b55db1d1aff794c
sinaendhran <sinaen@umich.edu>
2024-12-09 13:31:58 -0500

    Edited examples

--- examples/run_2dcases.py
+++ examples/run_2dcases.py
@@ -2,6 +2,5 @@ from simulateTestCases.run_sim import run_sim
 
 output_dir = 'output'
 sim = run_sim('simInfo.yaml', output_dir) # Input the simulation info and outptu dir
-sim.check_yaml_file() # Check the yaml_file
 sim.run_problem() # Run the simulation
 sim.post_process() # Genrates plots comparing experimental data and simulated data and stores them
