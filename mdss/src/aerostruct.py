01d1e5c51ca99e6258e503e3e2a91ae023b2df5f
sinaendhran <sinaen@umich.edu>
2025-11-09 00:06:35 -0500

    minor change to mdss problem

--- mdss/src/aerostruct.py
+++ mdss/src/aerostruct.py
@@ -231,7 +231,7 @@ class Top(Multipoint):
             ################################################################################
             aero_builder = ADflowBuilder(self.sim_info['aero_options'], scenario="aerostructural")
             aero_builder.initialize(self.comm)
-            aero_builder.err_on_convergence_fail = False
+            aero_builder.err_on_convergence_fail = True
             self.add_subsystem("mesh_aero", aero_builder.get_mesh_coordinate_subsystem())
             ################################################################################
             # TACS Setup
